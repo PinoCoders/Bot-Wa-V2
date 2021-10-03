b4ab0d2f60a5f522914b3521eebba1571d488855
PinoCoders <xn008uwuw@gmail.com>
2021-10-03 14:43:42 +0700

    first commit

---
+++ message/mess.js
@@ -0,0 +1,17 @@
+module.exports.mess = {
+	        wait: 'Ｌｏａｄｉｎｇ．．．',
+			success: 'Ｓｕｃｃｅｓ．．．',
+			wrongFormat: 'Format salah, coba liat lagi di menu',
+			error: {
+				api: 'Ups, terjadi kesalahan/Apikey Habis',
+				stick: 'bukan sticker itu:v',
+				Iv: 'Link tidak valid'
+			},
+			only: {
+				group: 'Maaf! Command ini khusus untuk di dalam Group saja.',
+				admin: 'Maaf! Command ini khusus untuk admin group saja.',
+				premium: 'Kamu bukan user premium, kirim perintah *!buypremium* untuk membeli premium',
+				owner: 'Maaf! Command ini khusus untuk Owner Bot saja.',
+				Badmin: 'Maaf! Command ini khusus untuk Bot ketika jadi admin!!',
+			}
+		}
